fe27297340e9325f3d19eff62ff1c0c4ff8fa596
Maulana Ibrahim <maulputra09@gmail.com>
2023-07-31 21:10:15 +0700

    fix error

--- controller/home.js
+++ controller/home.js
@@ -3,13 +3,14 @@ const Articles = require('../models/articles')
 const homeStartingContent = "Created By Amoeba Level Programmer Using MongoDB and Express";
 const secondDescription = "PERGI KE COMPOSE, ABIS ITU MASUKIN TITLE DAN DESCRIPTION SESUKA HATI!"
 
+
 exports.getHome = (req,res) => {
 
     Articles.find()
     .then(articles => {
         res.render('home', {
             pageTitle : 'Home',
-            SecondTitle : 'TUTORIAL CORET CORET DI WEB INI',
+            secondTitle : 'TUTORIAL CORET CORET DI WEB INI',
             content : homeStartingContent,
             secondDescription :secondDescription,
             articles : articles,
